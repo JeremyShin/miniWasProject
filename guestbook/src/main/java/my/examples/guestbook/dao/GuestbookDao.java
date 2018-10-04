824f2b8f5dacafa49cbd8646d5e5d9481f9a2193
jeremyshin <younchul.shin@gmail.com>
2018-10-05 01:51:33 +0900

    hide database id & password

--- guestbook/src/main/java/my/examples/guestbook/dao/GuestbookDao.java
+++ guestbook/src/main/java/my/examples/guestbook/dao/GuestbookDao.java
@@ -3,22 +3,21 @@ package my.examples.guestbook.dao;
 import my.examples.guestbook.servlet.Guestbook;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.Date;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GuestbookDao {
-    private String dbUrl="jdbc:mysql://localhost:3306/test?useSSL=false";
-    private String dbId="mysql";
-    private String dbPassword="mysql";
+    private String dbUrl = "jdbc:mysql://localhost:3306/test?useSSL=false";
+    private String dbId = "***";
+    private String dbPassword = "***";
 
     public List<Guestbook> getGuestbookList() {
         List<Guestbook> list = new ArrayList<>();
-
         Connection conn = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -29,39 +28,40 @@ public class GuestbookDao {
             ps = conn.prepareStatement(sql);
             rs = ps.executeQuery();
 
-            while(rs.next()){
+            while(rs.next()) {
                 Guestbook guestbook = new Guestbook();
                 guestbook.setId(rs.getLong(1));
                 guestbook.setName(rs.getString(2));
                 guestbook.setContent(rs.getString(3));
-                Date dbDate = rs.getDate(4);
-                java.util.Date date = new java.util.Date(dbDate.getTime()); // java.util.Date로변환.
-                LocalDateTime ldt = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+                Date dbDate = rs.getDate(4); // java.sql.Date
+                java.util.Date date = new java.util.Date(dbDate.getTime()); // java.util.Date로 변환
+                LocalDateTime ldt = date.toInstant()
+                        .atZone(ZoneId.systemDefault())
+                        .toLocalDateTime();
                 guestbook.setRegate(ldt);
                 list.add(guestbook);
+
             }
         }catch (Exception ex) {
             ex.printStackTrace();
         }finally {
             DbUtil.close(conn, ps, rs);
         }
-
         return list;
     }
 
-    public int addGuestbook(Guestbook guestbook){
+    public int addGuestbook(Guestbook guestbook) {
         int count = 0;
         Connection conn = null;
         PreparedStatement ps = null;
         try{
             conn = DbUtil.connect(dbUrl, dbId, dbPassword);
-            String sql = "insert into guestbook (id, name, content, regdate)\n" +
+            String sql = "insert into guestbook (id, name, content, regdate) \n" +
                     "values (null, ?, ?, now())";
             ps = conn.prepareStatement(sql);
             ps.setString(1, guestbook.getName());
             ps.setString(2, guestbook.getContent());
             count = ps.executeUpdate();
-
         }catch (Exception ex) {
             ex.printStackTrace();
         }finally {
@@ -70,11 +70,11 @@ public class GuestbookDao {
         return count;
     }
 
-    public int deleteGuestbook(Long id){
+    public int deleteGuestbook(Long id) {
         int count = 0;
         Connection conn = null;
         PreparedStatement ps = null;
-        try {
+        try{
             conn = DbUtil.connect(dbUrl, dbId, dbPassword);
             String sql = "delete from guestbook where id = ?";
             ps = conn.prepareStatement(sql);
